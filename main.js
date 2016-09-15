f4f380e2e499da6338467a7bddcf9af156d57bcf
Nicolas Oliver <nicolasoliver03@gmail.com>
2016-09-14 23:20:59 -0300

    add dinamic collection creation

--- main.js
+++ main.js
@@ -5,6 +5,18 @@ var router = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+
+/**
+ * Create Collection dinamically
+ */
+server.post('/:name', function (req, res) {
+  var obj = {};
+  obj[req.params.name] = [];
+  // by default create an empty table named `name`
+  router.db.defaults(obj).value();
+  res.sendStatus(201);
+});
+
 server.use(router);
 server.listen(process.env.PORT || 3000, function () {
   console.log('JSON Server is running');
